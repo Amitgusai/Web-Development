72fb7ce0d64fecc1968587028335e225bf754bdd
AmitGusai <amitgusai2003@gmail.com>
2023-12-17 16:40:41 +0530

    Getting started with Java

--- Java_Script/Lecture-1.js
+++ Java_Script/Lecture-1.js
@@ -1,4 +1,18 @@
-console.log("Journey to start learning JavaScript")
+// Used to declare a non-changeable Variable
+const Account_Id = 221022126461
+// Scope is not defined in this 'var' case
+var Account_city = "Almora" 
+// Scope is well-defined in case of 'let' keyword
+let Account_name = "Amit Gusai"
+// Not a good practice without using 'let' or 'const' keyword
+Account_state = "Uttarakhand" 
 
+// It will show undefined as it not a defined variable 
+let Account_pin 
+
+// To print 
+console.log(Account_Id);
+// To print multiple variable simultaneously , more Readability
+console.table([Account_Id, Account_city, Account_name, Account_state, Account_pin])
 
 
